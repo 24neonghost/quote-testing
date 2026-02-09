bef0b89540682cfde8a93ca8fa0b965d903eedcd
24sathvik <161731859+24sathvik@users.noreply.github.com>
2026-02-10 01:49:56 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,19 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-      {
-        protocol: 'http',
-        hostname: '**',
-      },
+      { protocol: "https", hostname: "**" },
+      { protocol: "http", hostname: "**" },
     ],
   },
+
   typescript: {
     ignoreBuildErrors: true,
   },
+
   eslint: {
     ignoreDuringBuilds: true,
   },
